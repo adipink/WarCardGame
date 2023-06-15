729003a237ab112fcbfc8a4880051af4a8d33eeb
kristina <kkristina.kolesnyk@gmail.com>
2023-06-15 12:56:03 +0300

    Initial Commit

---
+++ PlayingCardApp/PlayingCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingCardAppApp.swift
+//  PlayingCardApp
+//
+//  Created by Kristina on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlayingCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
